320afe0f8af2a948a3a737dc977ada1f3f43fdae
Maraka <rajamaraka446@gmail.com>
2026-09-25 11:40:09 +0530

    Create outputs.tf

---
+++ terraform/modules/networking/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "primary_subnet" {
+  value = google_compute_subnetwork.primary.name
+}
+
+output "secondary_subnet" {
+  value = google_compute_subnetwork.secondary.name
+}
+
+output "primary_pod_range" {
+  value = google_compute_subnetwork.primary.secondary_ip_range[0].range_name
+}
+
+output "primary_service_range" {
+  value = google_compute_subnetwork.primary.secondary_ip_range[1].range_name
+}
+
+output "secondary_pod_range" {
+  value = google_compute_subnetwork.secondary.secondary_ip_range[0].range_name
+}
+
+output "secondary_service_range" {
+  value = google_compute_subnetwork.secondary.secondary_ip_range[1].range_name
+}
