eb059939315c76c30e5ec29d3b0259ab4b8ad0bc
Maraka <rajamaraka446@gmail.com>
2026-09-25 11:39:31 +0530

    Add variables for networking module in Terraform

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,12 @@
+variable "network_name" {}
+variable "region" {}
+variable "secondary_region" {}
+
+variable "primary_subnet_cidr" {}
+variable "secondary_subnet_cidr" {}
+
+variable "pod_range_primary" {}
+variable "service_range_primary" {}
+
+variable "pod_range_secondary" {}
+variable "service_range_secondary" {}
